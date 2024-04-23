c6548a015e24994ae8b0492be938dc4ca47ed5d6
Michał Wójcik <michwoj01@gmail.com>
2024-04-23 18:51:44 +0200

    another Changes

--- genetic.py
+++ genetic.py
@@ -5,22 +5,44 @@ from scripts.compare_reports import compare_reports
 from scripts.add_observers import add_observers
 from scripts.parse_nodes import parse_nodes
 from deap import base, creator, tools, algorithms
+import random
 
 min_leak_coefficient = 0.0 
 max_leak_coefficient = 0.1
 
+def create_individual():
+    leak_node = np.random.choice(leak_names)
+    leak_coefficient = np.random.uniform(min_leak_coefficient, max_leak_coefficient)
+    return [leak_node, leak_coefficient]
+
 def evaluate(individual):
-    if not all(val > 0 for val in individual): 
-        return (1000,)
-    leak_dict = {leak_names[i]: individual[i] for i in range(len(leak_names))}
-    add_leaks(leak_dict, base_net, output_net)
+    leak_node, leak_coefficient = individual
+    add_leaks({leak_node: leak_coefficient}, base_net, output_net)
     os.system(f"{epanet_dir} {output_net} {output_report}")
     diff = compare_reports(model_report, output_report)
     return (diff,)
 
-def create_individual():
-    return creator.Individual(np.random.uniform(min_leak_coefficient, max_leak_coefficient) for _ in range(len(leak_names)))
+def custom_crossover(ind1, ind2, alpha=0.5):
+    """Funkcja krzyżowania dla osobników reprezentowanych przez listy."""
+    size = min(len(ind1), len(ind2))
+    cxpoint = np.random.randint(1, size)  # Losowy punkt krzyżowania
 
+    # Kopiowanie części osobników do zamiany
+    ind1_part = ind1[:cxpoint] + ind2[cxpoint:]
+    ind2_part = ind2[:cxpoint] + ind1[cxpoint:]
+
+    # Tworzenie nowych obiektów Individual
+    return creator.Individual(ind1_part), creator.Individual(ind2_part)
+
+def custom_mutation(individual, mu, sigma, indpb):
+    if random.random() < indpb:
+        node, coefficient = individual
+        print(node, coefficient)
+        coefficient += random.gauss(mu, sigma)
+        if coefficient < min_leak_coefficient:
+            coefficient = min_leak_coefficient
+        individual = (node, coefficient)
+    return creator.Individual(individual), 
 # Main
 if __name__ == "__main__":
     files_dir = "/home/michal/MiSS-EPANET-Leaks-in-waterworks-research-project/knowledge_sources/real_life_network_data/"
@@ -32,35 +54,37 @@ if __name__ == "__main__":
     model_report = f"{files_dir}model.txt"
     output_report = f"{files_dir}genetic.txt"
 
-    # manually add two leaks
-    emitters = {"RED1":0.02, "SW2": 0.05}
     observers = ["SW20", "HP12", "HP5", "SW/K01"]
+    emitters = {"SD19":0.05}
     add_observers(observers, base_net, observers_net)
     add_leaks(emitters, observers_net, model_net)
-    os.system(f"runepanet {model_net} {model_report}")
+    os.system(f"{epanet_dir} {model_net} {model_report}")
 
     # retrieve optimization parameters
     leak_names = parse_nodes(base_net)
 
-    toolbox = base.Toolbox()
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMin)
+    creator.create("Individual", tuple, fitness=creator.FitnessMin)
+    toolbox = base.Toolbox()
     toolbox.register("individual", create_individual)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("evaluate", evaluate)
-    toolbox.register("mate", tools.cxBlend, alpha=0.5)
-    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=0.1, indpb=0.2)
-    toolbox.register("select", tools.selTournament, tournsize=3)
+    toolbox.register("mate", custom_crossover)
+    toolbox.register("mutate", custom_mutation, mu=0.05, sigma=0.05, indpb=0.2)
+    toolbox.register("select", tools.selBest)
 
     population_size = 50
     num_generations = 20
-    pop = toolbox.population(n=population_size)
+    pop = [creator.Individual(toolbox.individual()) for _ in range(population_size)]
+    print("Initial population:")
+    for ind in pop:
+        print(ind)
     algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=num_generations, verbose=True)
     best_individual = tools.selBest(pop, k=1)[0]
-    optimal_leak_coefficients = {leak_names[i]: best_individual[i] for i in range(len(leak_names))}
+    best_leak_node, best_leak_coefficient = best_individual
     
-    add_leaks(optimal_leak_coefficients, base_net, output_net)
+    add_leaks({best_leak_node: best_leak_coefficient}, base_net, output_net)
     os.system(f"{epanet_dir} {output_net} {output_report}")
     diff = compare_reports(model_report, output_report)
-    print({k: v for k, v in optimal_leak_coefficients.items() if v > 0.0})
+    print(best_leak_node, best_leak_coefficient)
     print("Difference:", diff)
